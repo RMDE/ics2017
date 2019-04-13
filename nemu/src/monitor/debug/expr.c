c6b866ea64e85f51e36ed35f0f1fe72a0fb4daab
tracer-ics2017 <tracer@njuics.org>
2019-04-13 12:46:00 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  12:46:00 up  2:35,  2 users,  load average: 0.00, 0.00, 0.00 1ef72067cf8a079a17d8ad274f0b42cc22adf2bc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -34,15 +34,15 @@ static struct rule {
   {"\\)", TK_RPAR},						  // the right parenthese
   {"0x[0-9a-fA-F]{1,8}", TK_NUMBER_16},    // hexadecimal
   {"[0-9]+", TK_NUMBER_10},				  // decimalism
-  {"\\$eax", TK_EAX},                     // $eax
+  {"\\$eax|\\$EAX", TK_EAX},                     // $eax
   {"\\$ebx|\\$EBX", TK_EBX},                     // $ebx
-  {"\\$ecx\\|\\$ECX", TK_ECX},                     // $ecx
-  {"\\$edx\\|\\$EDX", TK_EDX},                     // $edx
-  {"\\$esp\\|\\$EBP", TK_ESP},                     // $esp
-  {"\\$ebp\\|\\$EBP", TK_EBP},                     // $ebp
-  {"\\$esi\\|\\$ESI", TK_ESI},                     // $esi
-  {"\\$edi\\|\\$EDI", TK_EDI},                     // $edi
-  {"\\$eip\\|\\$EIP", TK_EIP}                      // $eip
+  {"\\$ecx|\\$ECX", TK_ECX},                     // $ecx
+  {"\\$edx|\\$EDX", TK_EDX},                     // $edx
+  {"\\$esp|\\$EBP", TK_ESP},                     // $esp
+  {"\\$ebp|\\$EBP", TK_EBP},                     // $ebp
+  {"\\$esi|\\$ESI", TK_ESI},                     // $esi
+  {"\\$edi|\\$EDI", TK_EDI},                     // $edi
+  {"\\$eip|\\$EIP", TK_EIP}                      // $eip
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
