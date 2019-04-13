fdaade90fc8f21cd3a4910f157e80e9670cd4ac1
tracer-ics2017 <tracer@njuics.org>
2019-04-14 02:05:39 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  02:05:39 up  8:46,  2 users,  load average: 0.05, 0.04, 0.01 6400b5c4d33ec585fd101bcee0809f6ea0923b73

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -204,7 +204,7 @@ int find_dominated_op(int p,int q,bool *success)
               case TK_G:op=type>2? i:op;type=type>2? 2:type;break;
               case '+':op=type>3? i:op;type=type>3? 3:type;break;
               //case '-':op=type>3? i:op;type=type>3? 3:type;break;
-			  case '-':if(op==i-1);else{op=type>3? i:op;type=type>3? 3:type;}break;
+			  case '-':if(op==i-1||i==p);else{op=type>3? i:op;type=type>3? 3:type;}break;
               case '*':op=type>4? i:op;type=type>4? 4:type;break;
               case TK_DIV:op=type>4? i:op;type=type>4? 4:type;break;
               case TK_SHIFT_LEFT:op=type>5? i:op;type=type>5? 5:type;break;
