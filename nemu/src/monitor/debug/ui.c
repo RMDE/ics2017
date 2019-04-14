ba507315a5ff666bde835ca27692913c83e68216
tracer-ics2017 <tracer@njuics.org>
2019-04-14 11:24:42 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  11:24:42 up  2:43,  2 users,  load average: 0.03, 0.02, 0.00 fbae263427decdc5d21b471ed599866ea93e850

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -118,6 +118,7 @@ static int cmd_x(char *args){
 	int len=n-'0';
 	//printf("%d\n",len);
 	char *arg=strtok(NULL," ");
+	printf("%s\n",arg);
 	strtok(arg,"x");
 	arg=strtok(NULL," ");
 	unsigned int addr=0;
