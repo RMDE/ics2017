f6cc5860d328a06b96fec67f96fc0613bed968a6
tracer-ics2017 <tracer@njuics.org>
2019-04-14 10:17:11 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  10:17:11 up  1:35,  2 users,  load average: 0.11, 0.03, 0.01 9fac34f98dbf83c628917db6bdfc493477b345ec

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -179,7 +179,7 @@ bool check_parentheses(int p,int q,bool *success)
 
 int find_dominated_op(int p,int q,bool *success)
 {
-	printf("now in find");
+	//printf("now in find");
     int level =0;
     int op=-1;
     int type=15;//初始化
@@ -235,7 +235,7 @@ int find_dominated_op(int p,int q,bool *success)
 			}
 		}*/
     }
-	printf("%d\n",op);
+	//printf("%d\n",op);
     if(op==-1)
         *success=false;
     return op;
