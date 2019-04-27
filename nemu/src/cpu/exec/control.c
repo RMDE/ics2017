2efc88e8a1bf02d73ee7c4adc1b5c6886c2b9434
tracer-ics2017 <tracer@njuics.org>
2019-04-28 00:03:28 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  00:03:28 up  1:51,  2 users,  load average: 0.05, 0.03, 0.01 a5925f9f4fd23e31f6aec99b0c52ee85027945e

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -37,7 +37,7 @@ make_EHelper(call) {
 make_EHelper(ret) {
   //TODO();
   rtl_pop(&decoding.jmp_eip);
-  printf("ebp/return:%08x\n",decoding.jmp_eip);
+  //printf("ebp/return:%08x\n",decoding.jmp_eip);
   decoding.is_jmp=1;
 
   print_asm("ret");
