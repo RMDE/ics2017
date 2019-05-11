935b9b2d4a95318bcbeeae7aac17ccd066b55daf
tracer-ics2017 <tracer@njuics.org>
2019-05-11 09:20:10 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  09:20:09 up 29 min,  2 users,  load average: 0.00, 0.00, 0.00 af6ad4c5f0870791d13c6e4de37db5971072700a

--- nemu/src/cpu/exec/prefix.c
+++ nemu/src/cpu/exec/prefix.c
@@ -3,8 +3,10 @@
 make_EHelper(real);
 
 make_EHelper(operand_size) {
-  //decoding.is_operand_size_16 = true;
-  //exec_real(eip);
-  //decoding.is_operand_size_16 = false;
+  decoding.is_operand_size_16 = true;
+  printf("1");
+  exec_real(eip);
+  printf("2");
+  decoding.is_operand_size_16 = false;
 //	printf("this is in operand_size func\n");
 }
