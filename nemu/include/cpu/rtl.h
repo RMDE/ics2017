40342783b9946b698f5fa9a15170ea3e1a17c906
tracer-ics2017 <tracer@njuics.org>
2019-05-12 23:47:38 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  23:47:38 up 12:13,  3 users,  load average: 0.02, 0.29, 0.50 c5a9f5cd3eccedf2a47522977eab70cb2feb557c

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -153,7 +153,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   t0=c_sar(t0,t1); //符号右移
   printf("%08x\n",t0);
   if(t0==0x000000ff)
-	 cpu. eip=0x00000000;
+	  cpu.eax=0;
   *dest=t0;
 }
 
