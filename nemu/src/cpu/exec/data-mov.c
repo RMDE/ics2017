89fd79c2012a6f099f8cd76777a009b2637914fb
tracer-ics2017 <tracer@njuics.org>
2019-05-12 22:06:15 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  22:06:15 up 10:32,  2 users,  load average: 0.20, 0.31, 0.17 c437f3639e9cb380d98197ee6ebe3147fe22ae2c

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -67,9 +67,9 @@ make_EHelper(cwtl) {
 	rtl_sext(&t1,&cpu.eax,8);
 	rtl_slt(&t0,&t1,&tzero);
 	if(t0)
-		cpu.gpr[0]._8[1]=0xff;
+		cpu.gpr[0]._8[0]=0xff;
 	else
-		cpu.gpr[0]._8[1]=0x0;
+		cpu.gpr[0]._8[0]=0x0;
   }
   else {
     //TODO();
