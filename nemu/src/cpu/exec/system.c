d1c212beffa48cf8e665ded4978e02dfe80e8dd3
tracer-ics2017 <tracer@njuics.org>
2019-05-12 19:50:21 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  19:50:21 up  8:16,  2 users,  load average: 0.00, 0.00, 0.00 42f348d7b2018b7c2ac57d439baed7cb0f4a61e

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -46,7 +46,7 @@ void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
   //TODO();
-  t1=pio_read(id_src->val,id_dest->val);
+  t1=pio_read(id_src->val,id_dest->width);
   operand_write(id_dest,&t1);
 
   print_asm_template2(in);
