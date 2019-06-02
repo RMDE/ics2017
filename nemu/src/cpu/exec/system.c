26207183f8db4583345b317cfcee69272f445769
tracer-ics2017 <tracer@njuics.org>
2019-06-02 15:05:56 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  15:05:56 up 57 min,  2 users,  load average: 0.23, 0.11, 0.08 5f75d55b759e27144f2f32a0c5b94add3bddb121

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -5,7 +5,7 @@ void diff_test_skip_nemu();
 
 make_EHelper(lidt) {
   //TODO();
-  t0=id_dest->val;
+  t0=id_dest->addr;
   cpu.idtr.limit=vaddr_read(t0,2);
   printf("len: %08x\n",cpu.idtr.limit);
   if(id_dest->width==2)
