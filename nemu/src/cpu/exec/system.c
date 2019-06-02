54e0ee6d392cd3f34a43fb88ab14d9cc321bca01
tracer-ics2017 <tracer@njuics.org>
2019-06-02 14:22:42 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  14:22:42 up 14 min,  2 users,  load average: 0.00, 0.04, 0.01 2031bc0498c9c0424467019851a6c2aff8bf2064

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -29,8 +29,8 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-  TODO();
-  //raise_intr(id_dest->val,cpu.eip);
+  //TODO();
+  raise_intr(id_dest->val,cpu.eip);
 
   print_asm("int %s", id_dest->str);
 
