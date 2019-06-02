573110546b8f1f35d5529b809bda546d647be5bb
tracer-ics2017 <tracer@njuics.org>
2019-06-02 22:45:15 +0800

    >  gdb 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  22:45:15 up 19 min,  2 users,  load average: 0.21, 0.17, 0.06 5c1d02db5537313c6728e3bd49d5fc2f77b1cd4d

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -35,7 +35,7 @@ void _asye_init(_RegSet*(*h)(_Event, _RegSet*)) {
 
   // -------------------- system call --------------------------
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_USER);
-  printf("IDT[0x80]:%016x",&idt[128]);
+  printf("IDT[0x80]:%016x\n",idt[128]);
   set_idt(idt, sizeof(idt));
 
   // register event handler
