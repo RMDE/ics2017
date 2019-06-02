11e49d10ef723f6d60fe65cb0f3b1329f1543a64
tracer-ics2017 <tracer@njuics.org>
2019-06-02 22:58:27 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  22:58:27 up 32 min,  2 users,  load average: 0.22, 0.11, 0.08 7f2ef60cae7625325a018d57a6d38c93fd46f29

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -40,6 +40,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   eip_low=vaddr_read(idtr_base+NO*8,4)&0x0000ffff;
   eip_high=vaddr_read(idtr_base+NO*8+4,4)&0xffff0000;
   offset=eip_low|eip_high;
+  printf("%08x\n",offset);
   decoding.jmp_eip=offset;
   decoding.is_jmp=true;
 }
