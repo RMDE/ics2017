7c0a86cce5afbd778836d158125a57064cf3f4bd
tracer-ics2017 <tracer@njuics.org>
2019-06-02 23:20:49 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  23:20:49 up 55 min,  2 users,  load average: 0.10, 0.20, 0.14 718e38887f7ee0bd43aee73d335ccc082a45d0d5

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,9 +7,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
 
   //TODO();
-  printf("%08x\n",cpu.idtr.base);
   uint32_t addr=cpu.idtr.base+NO*8;
-  printf("%08x\n",addr);
   //GateDesc data;
   struct{
 	union {
@@ -22,31 +20,16 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   }data3;
   data3.data1=vaddr_read(addr,4);
   data3.data2=vaddr_read(addr+4,4);
-  printf("%08x %08x\n",data3.data1,data3.data2);
-  //if((data3.data.present&0x1)!=1)assert(0);
+  if((data3.data.present&0x1)!=1)assert(0);
   rtl_push(&cpu.flag);
   uint32_t cs=cpu.cs&0xffff;
   rtl_push(&cs);
   rtl_push(&ret_addr);
   addr=data3.data.offset_31_16&0xffff;
-  printf("%08x\n",data3.data.offset_31_16);
   addr<<=16;
   addr=addr|(data3.data.offset_15_0&0x0000ffff);
-  //cpu.eip=addr;
   decoding.jmp_eip=addr;
   decoding.is_jmp=true;
-  printf("%08x\n",addr);
-  /*rtl_push((rtlreg_t *)&cpu.flag);
-  rtl_push((rtlreg_t*)&cpu.cs);
-  rtl_push((rtlreg_t*)&ret_addr);
-  uint32_t idtr_base=cpu.idtr.base;
-  uint32_t eip_low,eip_high,offset;
-  eip_low=vaddr_read(idtr_base+NO*8,4)&0x0000ffff;
-  eip_high=vaddr_read(idtr_base+NO*8+4,4)&0xffff0000;
-  offset=eip_low|eip_high;
-  printf("%08x\n",offset);
-  decoding.jmp_eip=offset;
-  decoding.is_jmp=true;*/
 }
 
 void dev_raise_intr() {
