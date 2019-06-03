aa623a581b7c8928ff657a66622af6d9a0b344f4
tracer-ics2017 <tracer@njuics.org>
2019-06-03 14:17:51 +0800

    >  gdb 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  14:17:51 up  1:17,  3 users,  load average: 0.11, 0.14, 0.08 19898175b6c6fb8357ae6ac0d64c777a4359b60c

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,6 +3,7 @@
 _RegSet* do_syscall(_RegSet *r);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
+	printf("%d",e.event);
   switch (e.event) {
 //	case 8: do_syscall(r);printf("in 8\n");break;
     default: panic("Unhandled event ID = %d", e.event);
