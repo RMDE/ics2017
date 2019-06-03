619358a1b7f0018f849cbf8a0fe5fec50c4531fe
tracer-ics2017 <tracer@njuics.org>
2019-06-03 21:19:29 +0800

    >  compile 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  21:19:29 up  2:26,  2 users,  load average: 0.14, 0.11, 0.03 758e7a8d3dca66672f9fbdadd24564d7c0a41ab1

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -44,16 +44,16 @@ make_EHelper(int) {
 
 make_EHelper(iret) {
   //TODO();
-  printf("in iret");
+  printf("in iret\n");
   rtl_pop(eip);
-  printf("iret");
+  printf("iret\n");
   decoding.jmp_eip=*eip;
-  printf("iret");
+  printf("iret\n");
   decoding.is_jmp=true;
-  printf("iret");
+  printf("iret\n");
   rtl_pop((rtlreg_t*)&cpu.cs);
   rtl_pop(&cpu.flag);
-  printf("iret");
+  printf("iret\n");
 
   print_asm("iret");
 }
