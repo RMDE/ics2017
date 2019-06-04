5f8d2fc70f3abc21af034a1538a6566a6aa5afae
tracer-ics2017 <tracer@njuics.org>
2019-06-05 00:53:19 +0800

    >  gdb 161720110 yexi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  00:53:19 up  4:33,  3 users,  load average: 0.00, 0.00, 0.00 6021b613bc1748627dd2a02c4836c093b07428ea

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,12 +2,17 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
-void ramdisk_read(void*,off_t,size_t);
-size_t get_ramdisk_size();
+//void ramdisk_read(void*,off_t,size_t);
+int fs_open(const char*,int,int);
+ssize_t fs_read(int,void*,size_t);
+int fs_close(int);
+size_t fs_filesz(int);
+//size_t get_ramdisk_size();
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
-  size_t len;
-  len = get_ramdisk_size();
-  ramdisk_read(DEFAULT_ENTRY,0,len);
+  int num=fs_open(filename,0,0);
+  size_t len=fs_filesz(num);
+  fs_read(num,DEFAULT_ENTRY,len);
+  fs_close(num);
   return (uintptr_t)DEFAULT_ENTRY;
 }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,8 +7,8 @@ static inline uintptr_t sys_none(){
 }
 
 static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode) {
-  TODO();
-  return 1;
+  //TODO();
+  return fs_open((char*)pathname,0,0);
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
@@ -23,12 +23,17 @@ static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
 	  }
 	  return i;
   }
+  else{
+	  fs_write(fd,(void*)buf,(size_t)len);
+	  return len;
+  }
   return -1;
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
-  return 1;
+  //TODO();
+  fs_read(fd,(void*)buf,(size_t)len);
+  return len;
 }
 
 static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t whence) {
@@ -36,8 +41,8 @@ static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t when
 }
 
 static inline uintptr_t sys_close(uintptr_t fd) {
-  TODO();
-  return 1;
+  //TODO();
+  return fs_close(fd);
 }
 
 static inline uintptr_t sys_brk(uintptr_t new_brk) {
@@ -48,19 +53,21 @@ static inline uintptr_t sys_exit(int code){
 	_halt(code);
 	return 1;
 }
-void* sbrk(intptr_t increment);
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
-  //printf("%d %d %d %d\n",a[0],a[1],a[2],a[3]);
+  printf("%d %d %d %d\n",a[0],a[1],a[2],a[3]);
   switch (a[0]) {
-	case 0: SYSCALL_ARG1(r)=sys_none();break;
-	case 3: Log("call SYS_write");SYSCALL_ARG1(r)=sys_write(a[1],a[2],a[3]);break;
-	case 4: SYSCALL_ARG1(r)=sys_exit(0);break;
-	case 9: Log("call SYS_brk");SYSCALL_ARG1(r)=sys_brk(a[1]);break;
+	case SYS_none:  SYSCALL_ARG1(r)=sys_none();break;
+	case SYS_write: Log("call SYS_write");SYSCALL_ARG1(r)=sys_write(a[1],a[2],a[3]);break;
+	case SYS_exit:  SYSCALL_ARG1(r)=sys_exit(0);break;
+	case SYS_brk:   Log("call SYS_brk");SYSCALL_ARG1(r)=sys_brk(a[1]);break;
+	case SYS_open:  SYSCALL_ARG1(r)=sys_open(a[1],0,0);break;
+	case SYS_read:  SYSCALL_ARG1(r)=sys_read(a[1],a[2],a[3]);break;
+	case SYS_close: SYSCALL_ARG1(r)=sys_close(a[1]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   return NULL;
